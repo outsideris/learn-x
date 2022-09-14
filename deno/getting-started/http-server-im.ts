ec7af3693d9322e8a080069a97ff69cd4768f9bb
Outsider <outsideris@gmail.com>
2022-09-15 03:08:46 +0900

    deno: fmt
    Signed-off-by: Outsider <outsideris@gmail.com>

--- deno/getting-started/http-server-im.ts
+++ deno/getting-started/http-server-im.ts
@@ -2,7 +2,7 @@
 import { serve } from "http/server.ts";
 
 function handler(req: Request): Response {
-    return new Response("Hello, World!");
-  }
+  return new Response("Hello, World!");
+}
 
 serve(handler);
--- deno/getting-started/http-server.ts
+++ deno/getting-started/http-server.ts
@@ -2,7 +2,7 @@
 import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
 
 function handler(req: Request): Response {
-    return new Response("Hello, World!");
-  }
+  return new Response("Hello, World!");
+}
 
 serve(handler);
