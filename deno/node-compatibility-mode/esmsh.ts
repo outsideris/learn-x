beda9a8901f08a2dd1b543d151fd87fc280c55da
Outsider <outsideris@gmail.com>
2022-09-25 04:22:02 +0900

    deno: add cdn examples
    Signed-off-by: Outsider <outsideris@gmail.com>

---
+++ deno/node-compatibility-mode/esmsh.ts
@@ -0,0 +1,6 @@
+import * as chai from "https://esm.sh/chai@^4.3.6";
+
+const expect = chai.expect;
+
+const foo = 'foo';
+expect(foo).to.equal('bar');
---
+++ deno/node-compatibility-mode/skypack.ts
@@ -0,0 +1,6 @@
+import * as chai from "https://cdn.skypack.dev/chai?dts";
+
+const expect = chai.expect;
+
+const foo = 'foo';
+expect(foo).to.equal('bar');
