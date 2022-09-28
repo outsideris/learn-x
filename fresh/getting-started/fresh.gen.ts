2312ce163a5bb824a6cc5b0d5900db1b6670c3fb
Outsider <outsideris@gmail.com>
2022-09-29 03:35:27 +0900

    fresh: handers examples
    Signed-off-by: Outsider <outsideris@gmail.com>

--- fresh/getting-started/fresh.gen.ts
+++ fresh/getting-started/fresh.gen.ts
@@ -6,8 +6,9 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/about.tsx";
 import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/greet/[name].tsx";
-import * as $4 from "./routes/index.tsx";
+import * as $3 from "./routes/api/random-uuid.ts";
+import * as $4 from "./routes/greet/[name].tsx";
+import * as $5 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -15,8 +16,9 @@ const manifest = {
     "./routes/[name].tsx": $0,
     "./routes/about.tsx": $1,
     "./routes/api/joke.ts": $2,
-    "./routes/greet/[name].tsx": $3,
-    "./routes/index.tsx": $4,
+    "./routes/api/random-uuid.ts": $3,
+    "./routes/greet/[name].tsx": $4,
+    "./routes/index.tsx": $5,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
--- fresh/getting-started/routes/about.tsx
+++ fresh/getting-started/routes/about.tsx
@@ -1,3 +1,13 @@
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    const resp = await ctx.render();
+    resp.headers.set("X-Custom-Header", "Hello");
+    return resp;
+  },
+};
+
 export default function AboutPage() {
     return (
       <main>
